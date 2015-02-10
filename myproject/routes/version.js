1915338109e84db23acdede42406ba08c5d1d4b9
ningquanshui <ning.quanshui@hotmail.com>
2015-02-10 21:47:44 +0800

    kk

--- myproject/routes/version.js
+++ myproject/routes/version.js
@@ -5,6 +5,6 @@
 
 exports.list = function (req, res) {
     
-    res.send({ versionCode: 0, versionName: 'V1R01',  versionURL: 'http://121.199.59.67:8181/job/test/ws/compassdemo.apk' });
+    res.send({ versionCode: 0, versionName: 'V1.6',  versionURL: 'http://121.199.59.67:8181/job/test/ws/compassdemo.apk' });
 
 };
